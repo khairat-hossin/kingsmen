3a37060cd6043e6c0e8fef785113513fba93d937
Rejoan2014 <127096914+Rejoan2014@users.noreply.github.com>
2023-04-27 15:54:53 +0600

    contact create and read file added

--- app/Http/Controllers/Backend/Contacts/ContactController.php
+++ app/Http/Controllers/Backend/Contacts/ContactController.php
@@ -5,9 +5,42 @@ namespace App\Http\Controllers\Backend\Contacts;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
+    use Authorizable;
+
+    public $module_title;
+
+    public $module_name;
+
+    public $module_path;
+
+    public $module_icon;
+
+    public $module_model;
+
+    public function __construct()
+    {
+        // Page Title
+        $this->module_title = 'contacts';
+
+        // module name
+        $this->module_name = 'contacts';
+
+        // directory path of the module
+        $this->module_path = 'contacts';
+
+        // module icon
+        $this->module_icon = 'fas fa-donate';
+
+        // module model name, path
+        $this->module_model = "App\Models\Contact";
+    }
     /**
      * Display a listing of the resource.
      */
@@ -22,7 +55,18 @@ class ContactController extends Controller
      */
     public function create()
     {
-        //
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'Create';
+
+        Log::info(label_case($module_title.' '.$module_action).' | User:'.auth()->user()->name.'(ID:'.auth()->user()->id.')');
+
+        return view('backend.contacts.create', compact('module_title', 'module_name', 'module_icon', 'module_name_singular', 'module_action'));
     }
 
     /**
@@ -30,7 +74,39 @@ class ContactController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'Store';
+
+        $validatedData = $request->validate([
+            'first_name' => 'required|unique:'.$module_model.',first_name',
+            'last_name' => 'nullable:'.$module_model.',last_name',
+            'email' => 'nullable:'.$module_model.',email',
+            'phone' => 'nullable:'.$module_model.',phone',
+            'id_no' => 'nullable:'.$module_model.',id_no',
+        ]);
+
+        $contact = new Contact([
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
+            'email' => $validatedData['email'],
+            'phone' => $validatedData['phone'],
+            'id_no' => $validatedData['id_no']
+        ]);
+
+        $contact->save();
+
+
+        flash(icon().' '.Str::singular($module_title)."' Created.")->success()->important();
+
+        logUserAccess($module_title.' '.$module_action.' | Id: '.$$module_name_singular->id);
+
+        return redirect("admin/$module_name");
     }
 
     /**
---
+++ resources/views/backend/contacts/form.blade.php
@@ -0,0 +1,64 @@
+<div class="row mb-3">
+    <div class="col-12 col-md-6">
+        <div class="form-group">
+            <?php
+            $field_name = 'First Name';
+            $field_lable = label_case($field_name);
+            $field_placeholder = $field_lable;
+            $required = "required";
+            ?>
+            {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["name" => "first_name", "$required"]) }}
+        </div>
+    </div>
+    <div class="col-6 col-md-6">
+        <div class="form-group">
+            <?php
+            $field_name = 'Last Name';
+            $field_lable = label_case($field_name);
+            $field_placeholder = $field_lable;
+            $required = "";
+            ?>
+            {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["name" => "last_name", "$required"]) }}
+        </div>
+    </div>
+</div>
+<div class="row mb-3">
+    <div class="col-6 col-md-6">
+        <div class="form-group">
+            <?php
+            $field_name = 'Email';
+            $field_lable = label_case($field_name);
+            $field_placeholder = $field_lable;
+            $required = "";
+            ?>
+            {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["name" => "email", "$required"]) }}
+        </div>
+    </div>
+    <div class="col-6 col-md-6">
+        <div class="form-group">
+            <?php
+            $field_name = 'Phone';
+            $field_lable = label_case($field_name);
+            $field_placeholder = $field_lable;
+            $required = "";
+            ?>
+            {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["name" => "phone", "type" => "number", "$required"]) }}
+        </div>
+    </div>
+    <div class="col-6 col-md-6">
+        <div class="form-group">
+            <?php
+            $field_name = 'ID NO';
+            $field_lable = label_case($field_name);
+            $field_placeholder = $field_lable;
+            $required = "";
+            ?>
+            {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["name" => "id_no", "type" => "number", "$required"]) }}
+        </div>
+    </div>
+</div>
