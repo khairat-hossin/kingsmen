a049481546d7f50bf53ab74698a46fc0cbd233bc
Imran Hasan <imran.hasanpi@gmail.com>
2023-04-29 10:16:39 +0600

    teams create added

--- app/Http/Controllers/Backend/Team/TeamController.php
+++ app/Http/Controllers/Backend/Team/TeamController.php
@@ -5,9 +5,42 @@ namespace App\Http\Controllers\Backend\Team;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use App\Models\Team;
 
 class TeamController extends Controller
 {
+    use Authorizable;
+
+    public $module_title;
+
+    public $module_name;
+
+    public $module_path;
+
+    public $module_icon;
+
+    public $module_model;
+
+    public function __construct()
+    {
+        // Page Title
+        $this->module_title = 'Teams';
+
+        // module name
+        $this->module_name = 'teams';
+
+        // directory path of the module
+        $this->module_path = 'teams';
+
+        // module icon
+        $this->module_icon = 'fas fa-people-group';
+
+        // module model name, path
+        $this->module_model = "App\Models\Team";
+    }
     /**
      * Display a listing of the resource.
      */
@@ -22,7 +55,18 @@ class TeamController extends Controller
      */
     public function create()
     {
-        //
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'Create';
+
+        Log::info(label_case($module_title.' '.$module_action).' | User:'.auth()->user()->name.'(ID:'.auth()->user()->id.')');
+
+        return view('backend.teams.create', compact('module_title', 'module_name', 'module_icon', 'module_name_singular', 'module_action'));
     }
 
     /**
@@ -30,7 +74,79 @@ class TeamController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $module_action = 'store';
+        $request->validate([
+            "first_name"           => "required",
+            "last_name"            => "required",
+            "date_of_birth"        => "required",
+            "passport_number"      => "required|unique:" . $this->module_model . ",passport_number",
+            "passport_expiry_date" => "required",
+            "passport_photo"       => "required",
+            "ssn"                  => "required|unique:" . $this->module_model . ",ssn",
+            "id_card"              => "required",
+            "university_degree"    => "required",
+            "about_team_member"    => "required",
+            "position"             => "required",
+            "type"                 => "required",
+            "upload_photo"         => "required",
+            "personal_cell_number" => "required|unique:" . $this->module_model . ",personal_cell_number",
+            "personal_email"       => "required",
+            "home_address"         => "required",
+            "family_member_name"   => "required",
+            "work_contract"        => "required"
+        ]);
+        DB::beginTransaction();
+        // dd($request->all());
+        try {
+            $team  = new $this->module_model();
+            $passport_photo_url = '';
+            $id_card_url = '';
+            $upload_photo_url = '';
+
+            if ($request->passport_photo || $request->id_card || $request->upload_photo) {
+                $passport_photo_url = uploadFileToPublic($request->file('passport_photo'), 'teams/passport');
+                // $passport_photo_url = $request->file('passport_photo')->store('teams/passport', 'public');
+                dd($passport_photo_url);
+                $id_card_url = uploadFileToPublic($request->file('id_card'), 'teams/id_card');
+                $upload_photo_url = uploadFileToPublic($request->file('upload_photo'), 'teams/photo');
+            }
+
+            $team->first_name           = $request->first_name;
+            $team->last_name            = $request->last_name;
+            $team->date_of_birth        = $request->date_of_birth;
+            $team->passport_number      = $request->passport_number;
+            $team->passport_expiry_date = $request->passport_expiry_date;
+            $team->passport_photo       = $passport_photo_url;
+            $team->ssn                  = $request->ssn;
+            $team->id_card              = $id_card_url;
+            $team->university_degree    = $request->university_degree;
+            $team->about_team_member    = $request->about_team_member;
+            $team->position             = $request->position;
+            $team->type                 = $request->type;
+            $team->upload_photo         = $upload_photo_url;
+            $team->personal_cell_number = $request->personal_cell_number;
+            $team->personal_email       = $request->personal_email;
+            $team->assigned_cell_number = $request->assigned_cell_number;
+            $team->assigned_email       = $request->assigned_email;
+            $team->home_address         = $request->home_address;
+            $team->family_member_name   = $request->family_member_name;
+            $team->family_member_number   = $request->family_member_number;
+            $team->work_contract        = $request->work_contract;
+
+            $team->save();
+
+            flash(icon() . ' ' . Str::singular($this->module_title) . " Created Successfully")->success()->important();
+            logUserAccess($this->module_title . ' ' . $module_action . ' | Id: ' . $project->id);
+            DB::commit();
+            return redirect("admin/$this->module_name");
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            $msg = $th->getMessage();
+            flash(icon() . ' ' . Str::singular($this->module_title) . " Creation Failed! $msg")->error()->important();
+            return back()->withInput();
+        }
     }
 
     /**
