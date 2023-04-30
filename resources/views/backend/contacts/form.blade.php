a72c1d60f1b0e71fe4d2fe4b9aa7aa0c65985d11
Rejoan2014 <127096914+Rejoan2014@users.noreply.github.com>
2023-04-30 10:26:29 +0600

    Contact filed update

--- resources/views/backend/contacts/form.blade.php
+++ resources/views/backend/contacts/form.blade.php
@@ -127,7 +127,20 @@
             @enderror
         </div>
 </div>
-
+<div class="row mb-3">
+    <div class="col-6 col-md-4">
+        <div class="form-group">
+            <?php
+            $field_name = 'Date';
+            $field_lable = label_case($field_name);
+            $field_placeholder = $field_lable;
+            $required = "";
+            ?>
+            {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+            {{ html()->date($field_name)->placeholder($field_placeholder)->class('form-control form-control-sm')->attributes(["name" => "added_date", "$required"]) }}
+        </div>
+    </div>
+</div>
 <legend class="border-bottom w-100">Reffered By</legend>Country
 <div class="row mb-3">    
     <div class="col-6 col-md-4">
