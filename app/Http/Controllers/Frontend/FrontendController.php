9a28993af7a8422324ecebc45200bcefac07a759
Imran Hasan <imran.hasanpi@gmail.com>
2023-05-02 16:11:02 +0600

    project added to frontend

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Team;
+use App\Models\Project;
+use App\Models\Crowdfunding;
 
 class FrontendController extends Controller
 {
@@ -44,10 +46,24 @@ class FrontendController extends Controller
         return view('frontend.terms', compact('body_class'));
     }
 
-    public function teams()
+    public function team()
     {
          $teams = Team::all();
-         
-         return view('frontend.team.index', compact('teams'));
+
+         return view('frontend.team', compact('teams'));
+    }
+
+    public function projects()
+    {
+        $projects = Project::all();
+
+        return view('frontend.project', compact('projects'));
+    }
+
+    public function crowdfunding()
+    {
+        $crowdfundings = Crowdfunding::all();
+
+        return view('frontend.crowdfunding', compact('crowdfundings'));
     }
 }
