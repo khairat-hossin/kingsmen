b1fe5d1414817cc2e206e4b24b96ababb61bd3e5
Rejoan2014 <127096914+Rejoan2014@users.noreply.github.com>
2023-05-16 16:23:44 +0600

    team show file added

---
+++ resources/views/backend/teams/show.blade.php
@@ -0,0 +1,43 @@
+@extends('backend.layouts.app')
+
+@section('title') {{ __($module_action) }} {{ __($module_title) }} @endsection
+
+@section('breadcrumbs')
+<x-backend-breadcrumbs>
+    <x-backend-breadcrumb-item route='{{route("backend.$module_name.index")}}' icon='{{ $module_icon }}'>
+        {{ __($module_title) }}
+    </x-backend-breadcrumb-item>
+    <x-backend-breadcrumb-item type="active">{{ __($module_action) }}</x-backend-breadcrumb-item>
+</x-backend-breadcrumbs>
+@endsection
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <x-backend.section-header>
+            <i class="{{ $module_icon }}"></i> {{ __($module_title) }} <small class="text-muted">{{ __($module_action) }}</small>
+
+            <x-slot name="subtitle">
+                @lang(":module_name Management Dashboard", ['module_name'=>Str::title($module_name)])
+            </x-slot>
+            <x-slot name="toolbar">
+                <x-backend.buttons.return-back />
+                <x-buttons.show route='{!!route("backend.$module_name.show", $$module_name_singular)!!}' title="{{__('Show')}} {{ ucwords(Str::singular($module_name)) }}" class="ms-1" />
+            </x-slot>
+        </x-backend.section-header>
+
+        <hr>
+
+        <div class="row mt-4">
+            <div class="col">
+                {{ html()->modelForm($$module_name_singular, 'PATCH', route("backend.$module_name.update", $$module_name_singular))->class('form')->attribute('enctype', 'multipart/form-data')->open() }}
+
+                @include ("$module_path.$module_name.show_form")                
+
+            </div>
+        </div>
+    </div>
+    
+</div>
+
+@endsection
